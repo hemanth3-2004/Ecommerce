2a6ed602ccce8b35955b3c6b9154be69ff0f6c0f
hemanth3-2004 <nhemanth2004@gmail.com>
2025-04-13 01:04:24 +0530

    submenu and location added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,6 +4,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import CategoryModel from "./modals/CategoryModal";
 import SeperateModal from "./modals/seperateModal";
+import LocationModal from "./modals/LocationModal";
 function Header(){
     const [modal, setModal] = useState(false);
 
@@ -11,24 +12,19 @@ function Header(){
         setModal(!modal);
     }
 
-    const [type, setType] = useState(false);
+    const[location,setLocation] = useState(false);
 
-    function handleHoverEnter(){
-        setType(true);
+    function handleLocationClick(){
+        setLocation(!location);
     }
-
-    function handleHoverLeave(){
-        setType(false);
-    }
-
     return(
 
 
         <div className="py-7 pb-4">
             <div className="flex items-center  justify-around gap-6 ml-10 mr-10 pb-10">
                 <div className="ml-20"><img className="h-12 w-10/12" src="/ecommerce logo.jpg" alt="none" /></div>
-                <div className="flex items-center gap-4"><button className="border outline-none px-4 py-3 rounded-md  hover:bg-blue-100 transition-colors duration-500 hover:shadow-lg 
-                   transition-all duration-900">Your Location</button>
+                <div className="relative flex items-center gap-4"><button className="border outline-none px-4 py-3 rounded-md  hover:bg-blue-100 transition-colors duration-500 hover:shadow-lg 
+                   transition-all duration-900" onClick={handleLocationClick}>Your Location</button>
                 <div className="relative w-[500px]">
                  <input type="text" placeholder="Search for products..." className="w-full border outline-none px-4 py-3 pr-10 rounded-md bg-gray-200 placeholder-gray-500"/><SearchIcon className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 pointer-events-none" />
         </div>
@@ -40,7 +36,7 @@ function Header(){
                     <div className="ml-20"><button className="border outline-none rounded-full bg-custom text-white flex px-1 py-2 hover:bg-red-700" onClick={handleClick}><MenuIcon className="mr-1 ml-2"/><p className="text-sm">ALL CATEGORIES</p><KeyboardArrowDownIcon className="ml-1"/></button></div>
 
                      <div className="flex items-center justify-between gap-5 mr-10 ml-10">
-                     <div className=""><button className="rounded-full flex gap-2 px-2 py-1 text-gray-500 pl-5 pr-5 text-sm font-bold hover:bg-gray-100" onMouseEnter={handleHoverEnter} onMouseLeave={handleHoverLeave} ><img src="https://api.spicezgold.com/download/file_1734525204708_fash.png" width="25" height={10}  alt="fashion" /><p>FASHION</p></button></div>
+                     <div className=""><button className="rounded-full flex gap-2 px-2 py-1 text-gray-500 pl-5 pr-5 text-sm font-bold hover:bg-gray-100"><img src="https://api.spicezgold.com/download/file_1734525204708_fash.png" width="25" height={10}  alt="fashion" /><p>FASHION</p></button></div>
                      <div className=""><button className="rounded-full flex gap-2 px-2 py-1 text-gray-500 pl-5 pr-5 text-sm font-bold hover:bg-gray-100"><img src="https://api.spicezgold.com/download/file_1734525218436_ele.png" width="25" height={10}  alt="fashion" /><p>ELECTRONICS</p></button></div>                          
                      <div className=""><button className="rounded-full flex gap-2 px-2 py-1 text-gray-500 pl-5 pr-5 text-sm font-bold hover:bg-gray-100"><img src="https://api.spicezgold.com/download/file_1734525231018_bag.png" width="25" height={10}  alt="fashion" /><p>BAGS</p></button></div>
                      <div className=""><button className="rounded-full flex gap-2 px-2 py-1 text-gray-500 pl-5 pr-5 text-sm font-bold hover:bg-gray-100"><img src="https://api.spicezgold.com/download/file_1734525239704_foot.png" width="25" height={10}  alt="fashion" /><p>FOOTWEAR</p></button></div>
@@ -52,7 +48,14 @@ function Header(){
             </div>
 
             {modal && <CategoryModel />}
-            
+
+            {location && (
+                <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-start pt-32 z-50">
+                    <LocationModal closeLocation={handleLocationClick}/>
+                </div>
+            )}
+
+            <hr className="mt-5"/>
         </div>
     );
 }
--- client/src/components/modals/CategoryModal.jsx
+++ client/src/components/modals/CategoryModal.jsx
@@ -16,25 +16,26 @@ function CategoryModel(){
     return (
         <div className="relative">
         <div className="absolute flex flex-row">
-        <div className="ml-3 mt-3">
+        <div className="ml-3 mt-3 flex" onMouseEnter={handleHoverEnter} onMouseLeave={handleHoverLeave}>
  <div className="absolute border border-gray-300 w-40 ml-20 bg-white shadow-lg">
     
     <div className="flex flex-col justify-between ml-3 mt-1 mb-2">
-    <div className=""><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600" onMouseMove={handleHoverEnter} onMouseLeave={handleHoverLeave}><img src="https://api.spicezgold.com/download/file_1734525204708_fash.png" width="25" height={10}  alt="fashion" /><p>FASHION</p></button></div>
-    <div className=""><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600" onMouseEnter={handleHoverEnter} onMouseLeave={handleHoverLeave}><img src="https://api.spicezgold.com/download/file_1734525218436_ele.png" width="25" height={10}  alt="electronics" /><p>ELECTRONICS</p></button></div>                          
-    <div className=""><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600" onMouseEnter={handleHoverEnter} onMouseLeave={handleHoverLeave}><img src="https://api.spicezgold.com/download/file_1734525231018_bag.png" width="25" height={10}  alt="bags" /><p>BAGS</p></button></div>
-    <div className=""><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600" onMouseEnter={handleHoverEnter} onMouseLeave={handleHoverLeave}><img src="https://api.spicezgold.com/download/file_1734525239704_foot.png" width="25" height={10}  alt="footwear" /><p>FOOTWEAR</p></button></div>
-    <div className=""><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600" onMouseEnter={handleHoverEnter} onMouseLeave={handleHoverLeave}><img src="https://api.spicezgold.com/download/file_1734525248057_gro.png" width="25" height={10}  alt="groceries" /><p>GROCERIES</p></button></div>
-    <div className=""><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600" onMouseEnter={handleHoverEnter} onMouseLeave={handleHoverLeave}><img src="https://api.spicezgold.com/download/file_1734525255799_beauty.png" width="25" height={10}  alt="beauty" /><p>BEAUTY</p></button></div>
-    <div className=""><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600" onMouseEnter={handleHoverEnter} onMouseLeave={handleHoverLeave}><img src="https://api.spicezgold.com/download/file_1734525275367_well.png" width="25" height={10}  alt="wellness" /><p>WELLNESS</p></button></div>
- </div>
+    <div><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600"><img src="https://api.spicezgold.com/download/file_1734525204708_fash.png" width="25" height={10}  alt="fashion" /><p>FASHION</p></button></div>
+    <div><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600"><img src="https://api.spicezgold.com/download/file_1734525218436_ele.png" width="25" height={10}  alt="electronics" /><p>ELECTRONICS</p></button></div>                          
+    <div><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600"><img src="https://api.spicezgold.com/download/file_1734525231018_bag.png" width="25" height={10}  alt="bags" /><p>BAGS</p></button></div>
+    <div><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600"><img src="https://api.spicezgold.com/download/file_1734525239704_foot.png" width="25" height={10}  alt="footwear" /><p>FOOTWEAR</p></button></div>
+    <div><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600"><img src="https://api.spicezgold.com/download/file_1734525248057_gro.png" width="25" height={10}  alt="groceries" /><p>GROCERIES</p></button></div>
+    <div><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600"><img src="https://api.spicezgold.com/download/file_1734525255799_beauty.png" width="25" height={10}  alt="beauty" /><p>BEAUTY</p></button></div>
+    <div><button className=" flex gap-2 px-2 py-1 text-gray-500 text-sm hover:text-blue-600"><img src="https://api.spicezgold.com/download/file_1734525275367_well.png" width="25" height={10}  alt="wellness" /><p>WELLNESS</p></button></div>
  </div>
  </div>
 
- <div>
+
+
     {type && 
- <div className="border border-gray-300 w-40 bg-white shadow-lg h-[245px] ml-60 mt-3"><div><SubMenu /></div></div>
+ (<div className="border border-gray-300 w-40 bg-white shadow-lg h-[245px] ml-60"><div><SubMenu /></div></div>)
  }
+
 </div>
 </div>
 </div>

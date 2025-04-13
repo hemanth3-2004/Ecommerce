afb77a2deb9c6cddad7dd8b6f5a0464b75ea22e0
Nikhil <nikhil.4002.50.82@gmail.com>
2025-04-13 14:01:58 +0530

    finshed featurred animation

--- client/src/components/HomePage/FeaturedCategoriesComponent.jsx
+++ client/src/components/HomePage/FeaturedCategoriesComponent.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
-const FeaturedCategoriesComponent = ({name,src,bg}) => {
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
+const FeaturedCategoriesComponent = ({ name, src, bg }) => {
+  const img = useRef();
+  const tl1=gsap.timeline({paused:true})
+  useGSAP(() => {
+    tl1.to(img.current,{
+        y:20
+    })
+      tl1.to(
+        img.current,
+        {
+          y: -7,
+          duration: 1.5,
+          ease: "bounce.out",
+          repeat: -1,
+          yoyo:true
+        },
+        []
+      );
+  });
   return (
-    <div className="flex flex-col items-center justify-center mb-2 p-2">
-      <div className={`h-[7.5em] w-[7.5em] rounded-[50%] flex items-center justify-center  mb-1`} style={{backgroundColor:bg}}><img src={src} alt="" className="h-[5em] w-[6em] rounded-[50%]"/></div>
-      <div className="flex items-center justify-center"><p className="text-md font-semibold">{name}</p></div>
+    <div className="flex flex-col items-center justify-center mb-2 py-3">
+      <div
+        className={`h-[7.8em] w-[7.8em] rounded-[50%] flex items-center justify-center mb-1 hover:shadow-[0_4px_8px_0_rgba(0,0,0,0.2),0_6px_20px_0_rgba(0,0,0,0.19)]`}
+        style={{ backgroundColor: bg }}
+        onMouseEnter={()=>tl1.restart()}
+        onMouseLeave={()=>tl1.pause()}
+      >
+        <img
+          ref={img}
+          src={src}
+          alt=""
+          className="h-[5em] w-[6em] rounded-[50%]"
+          onMouseEnter={()=>setAnimation(true)}
+          onMouseLeave={()=>setAnimation(false)}
+        />
+      </div>
+      <div className="flex items-center justify-center">
+        <p className="text-md font-semibold">{name}</p>
+      </div>
     </div>
   );
 };

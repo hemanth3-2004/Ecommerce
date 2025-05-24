2149bfb990e1b75b252393a244a9fdeb48d77115
hemanth3-2004 <nhemanth2004@gmail.com>
2025-05-24 22:45:09 +0530

    Update Cart

--- client/src/components/header/Cart.jsx
+++ client/src/components/header/Cart.jsx
@@ -3,24 +3,27 @@ import CartProducts from './cartProducts';
 
 function Cart() {
   return (
-    <div className="bg-white text-gray-900 h-[80vh]">
-        <div className='border border-2 border-solid border-green-500 ml-[5%] mr-[5%] mt-[2%] py-2 h-auto bg-gray-100 rounded-md' >
-      <h1 className='border border-2 border-solid border-red-500 text-[1.5rem] font-bold ml-2.5'>Cart Items</h1>
-      <div className='flex flex-row mt-[1%] gap-[1%]'>
-      <div className=' w-[68%] ml-[1%] h-[100vh] rounded-md '>
+    <div className="bg-white text-gray-700 h-[80vh]">
+        <div className=' ml-[5%] mr-[5%] mt-[2%] py-2 h-auto bg-gray-100 rounded-md shadow-md shadow-gray-400' >
+      <h1 className=' text-[1.5rem] font-bold ml-5'>Cart Items</h1>
+      <div className='flex flex-row  gap-[1%]'>
+      <div className=' w-[73%] ml-[1%] h-[100vh] rounded-md '>
         <CartProducts />
         <CartProducts />
         <CartProducts />
       </div>
-      <div className='border border-2 border-solid border-red-500 w-[29%] mr-[1%] h-fit rounded-md bg-white h-[40vh] mt-[1%] shadow-md shadow-gray-400'>
+      <div className=' w-[25%] mr-[1%] h-fit rounded-md bg-white h-[40vh] mt-[1%] shadow-md shadow-gray-400'>
        <h2 className="text-center font-medium text-[1.3rem] my-1">Products</h2>
 
-       <div className='border border-2 border-solid border-red-500 w-[96%] ml-[2%] h-[80%] rounded-md '>
-        <div className='border border-2 border-solid border-green-500 my-[1%] mx-[.5%] flex flex-row'>
-            <div className='border border-2 border-solid border-blue-400 w-[45%] h-auto '>
-                <img className="h-[6rem] w-[90%] mx-[5%]" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
+       <div className=' w-[90%] ml-[4%] h-[80%] rounded-md shadow-md shadow-gray-400'>
+        <div className=' my-[1%] mx-[.5%] flex flex-row '>
+            <div className=' w-[45%] h-auto ml-[3%]'>
+                <img className="h-[5rem] w-[85%] mx-[5%]" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
             </div>
-            <div></div>
+            <div className='w-[45%] h-auto mr-[3%] flex flex-col items-center justify-around'>
+                 <h2 className='text-[1.1rem] text-center font-semibold'>Quantity : 2</h2>
+                 <h3 className='text-[1.1rem] text-center font-semibold'>₹109.95</h3>
+            </div>
         </div>
        </div>
       </div>

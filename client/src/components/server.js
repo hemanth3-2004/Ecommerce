90de8487c7a8243eaff6e139f5baa6b866e48b37
hemanth3-2004 <nhemanth2004@gmail.com>
2025-06-20 19:14:46 +0530

    vercel

--- client/src/components/server.js
+++ client/src/components/server.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
+   connectionString: process.env.DATABASE_URL,
     user : "postgres",
     host : "localhost",
     database : "ecommerce",
